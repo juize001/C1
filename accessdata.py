e5cd7fedd6d502a941b9a94ce5aaf2b455e0640b
zifei <zifttroll2004@gmail.com>
2025-10-16 12:36:53 +0100

    commit

---
+++ accessdata.py
@@ -0,0 +1,28 @@
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+# with open('LHCb/rapidsim_JpsiK.pkl', 'rb') as infile:
+#     data_jpsik = pickle.load(infile)
+# with open('LHCb/rapidsim_Kmumu.pkl', 'rb') as infile:
+#     data_kmumu = pickle.load(infile)
+# with open('LHCb/dataset_2011.pkl', 'rb') as infile:
+#     data_2011 = pickle.load(infile)
+# with open('LHCb/samesign_2012.pkl', 'rb') as infile:
+#     samesign_2011 = pickle.load(infile)
+
+data_2012u = pd.read_pickle('LHCb/dataset_2012_MagnetUp.pkl')
+data_2012d = pd.read_pickle('LHCb/dataset_2012_MagnetDown.pkl')
+data_2012 = pd.concat([data_2012u, data_2012d])
+# fs_data = pd.read_pickle("ProcessedData/first_selection_data.pkl")
+# print(len(samesign_2011['B invariant mass']))
+
+for item in list(data_2012d):
+    print(item)
+
+# plt.hist(fs_data['dimuon_system_invariant_mass'], bins=1000)
+# plt.hist(data_2012d['Magnet polarity'], bins=10)
+# plt.hist(data_2012u['Magnet polarity'], bins=10)
+plt.hist(data_2012['dimuon-system invariant mass'], bins=1000)
+# plt.hist(fs_data['Kaon_PID_NN_score_for_muon_hypothesis'], bins=100)
+plt.grid()
+plt.show()
