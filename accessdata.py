822c7f9567e1b6d4a36d84562b3fb20be0b2a657
zifei <zifttroll2004@gmail.com>
2025-10-17 15:03:28 +0100

    restyle labels

--- accessdata.py
+++ accessdata.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 # with open('LHCb/rapidsim_JpsiK.pkl', 'rb') as infile:
 #     data_jpsik = pickle.load(infile)
 # with open('LHCb/rapidsim_Kmumu.pkl', 'rb') as infile:
@@ -18,11 +19,10 @@ data_2012 = pd.concat([data_2012u, data_2012d])
 
 for item in list(data_2012d):
     print(item)
-
+data_2012.columns = [re.sub(r'[^A-Za-z0-9_]', '_', c) for c in data_2012.columns]
 # plt.hist(fs_data['dimuon_system_invariant_mass'], bins=1000)
 # plt.hist(data_2012d['Magnet polarity'], bins=10)
 # plt.hist(data_2012u['Magnet polarity'], bins=10)
-plt.hist(data_2012['dimuon-system invariant mass'], bins=1000)
-# plt.hist(fs_data['Kaon_PID_NN_score_for_muon_hypothesis'], bins=100)
+plt.hist(data_2012['Kaon_PID_NN_score_for_muon_hypothesis'], bins=100)
 plt.grid()
 plt.show()
