f8dfc881a20a1063b2f491c844d98c7f8e83e556
zifei <zifttroll2004@gmail.com>
2025-10-21 11:45:41 +0100

    code responsible for producing final data and its visuals are now in a function

--- signal_enhance.py
+++ signal_enhance.py
@@ -4,17 +4,74 @@ import re
 import matplotlib.pyplot as plt
 import joblib
 
-########## TO DO LIST - SELECTIONS - https://arxiv.org/pdf/1408.0978
-# 1. (done) Apply cut to D meson resonance range (1850-1880MeV)
-# 2. (done) Veto Charmonium backgrounds from hadron misidentification to muon
-# 3. (done) Veto some particle misidentifications e.g. kaons and pions
-# 4. (done) Veto kaon and muon swapped mass of jpsik decay
-# 5. Research standard model predictions for CP asymmetry based on dimuon mass squared
-# 6. Test the fit bias e.g. generate some data based on fit PDF and calculate the goodness of fit for each set
-# 7. Quantify accuracy and any biases of the ML model, include statistics and graphs
+########## TO DO LIST - SELECTIONS - https://arxiv.org/pdf/1408.0978 - https://journals.aps.org/prl/pdf/10.1103/PhysRevLett.111.151801
+# 1. Deal with unknown peak/resonance in Kmu mass plotted under dimuon mass hypothesis
+# 2. Get optimised model parameters and threshold
+# 3. Calculate ACP value using formula given in papers (weighted ACP)
+# 4. Get uncertainties from different fit functions
 
 # detector resolution for invariant mass - https://lhcb.web.cern.ch/speakersbureau/html/PerformanceNumbers.html
 # paper on LHCb setup and detector performance - https://iopscience.iop.org/article/10.1088/1748-0221/3/08/S08005/pdf
+
+def calc_acp_q2_bins(data, visual=False):
+    binned_selected_data, _, bin_bounds = split_into_q2_bins(data.copy())
+    results = []
+    for bin_idx, dat in binned_selected_data.items():  # .items() gives both key and value
+        dat = post_selection_vetoes(dat)
+        if dat.empty:
+            continue
+        try:
+            A_raw, A_raw_err, val_Np, val_Nm, *_ = fit_asymmetry_cb(dat)
+        except Exception as e:
+            print(f"Bin {bin_idx} failed: {e}")
+            continue
+        A_raw = A_raw - jpsik_acp
+        A_raw_err = np.sqrt(A_raw_err ** 2 + jpsik_acp_err ** 2)
+
+        q2_low = bin_bounds[bin_idx]
+        q2_high = bin_bounds[bin_idx + 1]
+        q2_center = 0.5 * (q2_low + q2_high)
+
+        results.append({
+            "q2_bin": bin_idx,
+            "q2_low": q2_low,
+            "q2_high": q2_high,
+            "q2_center": q2_center,
+            "A_raw": A_raw,
+            "A_raw_err": A_raw_err,
+            "Nsig_plus": val_Np,
+            "Nsig_minus": val_Nm,
+            "n_events": len(dat)
+        })
+    binned_data_all = pd.DataFrame(results).sort_values("q2_center").reset_index(drop=True)
+    if visual:
+        plt.errorbar(binned_data_all["q2_center"], binned_data_all["A_raw"],
+                    yerr=binned_data_all["A_raw_err"], fmt='o', capsize=4)
+        for _, row in binned_data_all.iterrows():
+            plt.text(
+                row["q2_center"], row["A_raw"] + 0.005,
+                f"N⁺={int(row['Nsig_plus'])}\nN⁻={int(row['Nsig_minus'])}",
+                ha='center', va='bottom', fontsize=7, color='dimgray'
+            )
+        plt.axhline(y=0, color='red', linestyle='--', linewidth=1)
+        plt.axhline(y=A_raw_tot, color='black', linestyle='--', linewidth=1)
+        plt.fill_between(
+            binned_data_all["q2_center"],
+            A_raw_tot - A_raw_err_tot,  # lower edge of band
+            A_raw_tot + A_raw_err_tot,  # upper edge of band
+            color='gray',
+            alpha=0.2,
+            label=r'$\pm 0.02$ uncertainty zone'
+        )
+        for xval in [8, 11, 12.5, 15]:
+            plt.axvline(x=xval, color='red', linestyle='-', linewidth=1)
+        plt.xlabel("Dimuon Mass Squared")
+        plt.ylabel("Raw Asymmetry  $A_{raw}$")
+        plt.title("Corrected Asymmetry vs Dimuon Mass Squared")
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
+    return binned_data_all
 
 from analysis_func import *
 
@@ -116,75 +173,18 @@ if 1 == 2:
     exit()
 
 
-# from zfit_func import *
-# from zfit_func_pulls import *
+from zfit_func_pulls import *
 # high_conf_signal = high_conf_signal[(high_conf_signal['B_invariant_mass'] > 5000) & (high_conf_signal['B_invariant_mass'] < 5500)]
 # sss = high_conf_signal[(high_conf_signal['B_invariant_mass'] < 5700) & (high_conf_signal['B_invariant_mass'] > 5150)].copy()
-high_conf_signal_with_vetoes = post_selection_vetoes(high_conf_signal.copy(), diagnostics=True)
-exit()
+high_conf_signal_with_vetoes = post_selection_vetoes(high_conf_signal.copy())
 
 A_raw_tot, A_raw_err_tot, *_ = fit_asymmetry_cb(high_conf_signal_with_vetoes.copy())
 A_raw_tot = A_raw_tot - jpsik_acp
 A_raw_err_tot = np.sqrt(A_raw_err_tot ** 2 + jpsik_acp_err ** 2)
 print(f'Corrected CP Asymmetry (raw) for Kmumu decay is {A_raw_tot} +- {A_raw_err_tot}')
+binned_data_all = calc_acp_q2_bins(high_conf_signal_with_vetoes, diagnostics=True)
 
-binned_selected_data, _, bin_bounds = split_into_q2_bins(high_conf_signal_with_vetoes.copy())
-results = []
-for bin_idx, dat in binned_selected_data.items():  # .items() gives both key and value
-    dat = kmu_mass_filter(dat)
-    if dat.empty:
-        continue
-    try:
-        A_raw, A_raw_err, val_Np, val_Nm = fit_asymmetry_cb(dat)
-    except Exception as e:
-        print(f"Bin {bin_idx} failed: {e}")
-        continue
-    A_raw = A_raw - jpsik_acp
-    A_raw_err = np.sqrt(A_raw_err ** 2 + jpsik_acp_err ** 2)
 
-    q2_low = bin_bounds[bin_idx]
-    q2_high = bin_bounds[bin_idx + 1]
-    q2_center = 0.5 * (q2_low + q2_high)
-
-    results.append({
-        "q2_bin": bin_idx,
-        "q2_low": q2_low,
-        "q2_high": q2_high,
-        "q2_center": q2_center,
-        "A_raw": A_raw,
-        "A_raw_err": A_raw_err,
-        "Nsig_plus": val_Np,
-        "Nsig_minus": val_Nm,
-        "n_events": len(dat)
-    })
-binned_data_all = pd.DataFrame(results).sort_values("q2_center").reset_index(drop=True)
-plt.errorbar(binned_data_all["q2_center"], binned_data_all["A_raw"],
-             yerr=binned_data_all["A_raw_err"], fmt='o', capsize=4)
-for _, row in binned_data_all.iterrows():
-    plt.text(
-        row["q2_center"], row["A_raw"] + 0.005,
-        f"N⁺={int(row['Nsig_plus'])}\nN⁻={int(row['Nsig_minus'])}",
-        ha='center', va='bottom', fontsize=7, color='dimgray'
-    )
-plt.axhline(y=0, color='red', linestyle='--', linewidth=1)
-plt.axhline(y=A_raw_tot, color='black', linestyle='--', linewidth=1)
-plt.fill_between(
-    binned_data_all["q2_center"],
-    A_raw_tot - A_raw_err_tot,  # lower edge of band
-    A_raw_tot + A_raw_err_tot,  # upper edge of band
-    color='gray',
-    alpha=0.2,
-    label=r'$\pm 0.02$ uncertainty zone'
-)
-for xval in [8, 11, 12.5, 15]:
-    plt.axvline(x=xval, color='red', linestyle='-', linewidth=1)
-plt.xlabel("Dimuon Mass Squared")
-plt.ylabel("Raw Asymmetry  $A_{raw}$")
-plt.title("Corrected Asymmetry vs Dimuon Mass Squared")
-plt.grid()
-plt.tight_layout()
-plt.show()
-exit()
 
 # y_score_train = lgbm.predict(X_train)
 # y_score_test  = lgbm.predict(X_test)
